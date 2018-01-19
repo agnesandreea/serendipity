d27280763ba9e07eaede4fbda79fd4784ebfa3a6
agnesandreea <agnes.andreea@gmail.com>
2018-01-19 18:10:12 +0200

    Create password.php

---
+++ password.php
@@ -0,0 +1,17 @@
+<html>
+<head>Special</head>
+<body>
+<?
+$age = $_POST['password'];
+
+if($password === likeariver) {
+echo 'x';
+}
+
+if ($password !== $likeariver) {
+echo 'y';
+}
+
+?>
+</body>
+</html>

74ccb2d3e90ceeeeae2d9d55ad2a6ce980b41a09
agnesandreea <agnes.andreea@gmail.com>
2018-01-19 17:48:38 +0200

    Create special.php

---
+++ special.php
@@ -0,0 +1,13 @@
+<html>
+<head> Special </head>
+
+<body>
+
+<?php
+<form action="message.php" method="post">
+Enter password: <input name="password" size="10">
+<input type="submit" value="Send">?>
+
+</body>
+
+</html>
